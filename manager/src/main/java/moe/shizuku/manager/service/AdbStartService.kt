1d3c6be67fffcbb209be60ca3facecec982a2e55
Ryfter <ryfterwastaken@gmail.com>
2025-07-01 20:28:50 +0100

    Using broadcast receiver instead of network callback as it is more reliable

--- manager/src/main/java/moe/shizuku/manager/service/AdbStartService.kt
+++ manager/src/main/java/moe/shizuku/manager/service/AdbStartService.kt
@@ -1,15 +1,18 @@
 package moe.shizuku.manager.service
 
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.IBinder
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import kotlinx.coroutines.CoroutineScope
@@ -26,71 +29,80 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 class AdbStartService : Service() {
-    lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    lateinit var cm: ConnectivityManager
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate() {
         super.onCreate()
 
-        val networkRequest = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .build()
+        cm = getSystemService(ConnectivityManager::class.java)
 
-        networkCallback = object : ConnectivityManager.NetworkCallback() {
-            @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
+        if (checkWifiConnected()) {
+            startShizuku(applicationContext)
+            return
+        }
 
-                Toast.makeText(applicationContext, R.string.notification_service_starting, Toast.LENGTH_SHORT).show()
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (context == null) return
 
-                val cr = applicationContext.contentResolver
-                Settings.Global.putInt(cr, "adb_wifi_enabled", 1)
-                Settings.Global.putInt(cr, Settings.Global.ADB_ENABLED, 1)
-                Settings.Global.putLong(cr, "adb_allowed_connection_time", 0L)
-                CoroutineScope(Dispatchers.IO).launch {
-                    val latch = CountDownLatch(1)
-                    val adbMdns = AdbMdns(applicationContext, AdbMdns.TLS_CONNECT) { port ->
-                        if (port <= 0) return@AdbMdns
-                        try {
-
-                            val keystore = PreferenceAdbKeyStore(ShizukuSettings.getPreferences())
-                            val key = AdbKey(keystore, "shizuku")
-                            val client = AdbClient("127.0.0.1", port, key)
-                            client.connect()
-                            client.shellCommand(Starter.internalCommand, null)
-                            client.close()
-
-                            Settings.Global.putInt(cr, "adb_wifi_enabled", 0)
-
-                            stopSelf()
-
-                        } catch (_: Exception) {}
-                        latch.countDown()
-                    }
-                    if (Settings.Global.getInt(cr, "adb_wifi_enabled", 0) == 1) {
-                        adbMdns.start()
-                        latch.await(5, TimeUnit.SECONDS)
-                        adbMdns.stop()
-                    }
+                if (checkWifiConnected()) {
+                    startShizuku(context)
+                    unregisterReceiver(this)
                 }
             }
         }
+        val intentFilter = IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION)
 
-        val cm = getSystemService(ConnectivityManager::class.java)
-        cm.registerNetworkCallback(networkRequest, networkCallback)
+        applicationContext.registerReceiver(receiver, intentFilter)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return START_STICKY
     }
 
-    override fun onDestroy() {
-        val cm = getSystemService(ConnectivityManager::class.java)
-        cm.unregisterNetworkCallback(networkCallback)
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun startShizuku(context: Context) {
+        Toast.makeText(context, R.string.notification_service_starting, Toast.LENGTH_SHORT).show()
 
-        super.onDestroy()
+        val cr = context.contentResolver
+        Settings.Global.putInt(cr, "adb_wifi_enabled", 1)
+        Settings.Global.putInt(cr, Settings.Global.ADB_ENABLED, 1)
+        Settings.Global.putLong(cr, "adb_allowed_connection_time", 0L)
+        CoroutineScope(Dispatchers.IO).launch {
+            val latch = CountDownLatch(1)
+            val adbMdns = AdbMdns(context, AdbMdns.TLS_CONNECT) { port ->
+                if (port <= 0) return@AdbMdns
+                try {
+
+                    val keystore = PreferenceAdbKeyStore(ShizukuSettings.getPreferences())
+                    val key = AdbKey(keystore, "shizuku")
+                    val client = AdbClient("127.0.0.1", port, key)
+                    client.connect()
+                    client.shellCommand(Starter.internalCommand, null)
+                    client.close()
+
+                    Settings.Global.putInt(cr, "adb_wifi_enabled", 0)
+
+                    stopSelf()
+
+                } catch (_: Exception) {}
+                latch.countDown()
+            }
+            if (Settings.Global.getInt(cr, "adb_wifi_enabled", 0) == 1) {
+                adbMdns.start()
+                latch.await(5, TimeUnit.SECONDS)
+                adbMdns.stop()
+            }
+        }
+    }
+
+    private fun checkWifiConnected() : Boolean {
+        return cm.getNetworkCapabilities(cm.activeNetwork)
+            ?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true
     }
 }
