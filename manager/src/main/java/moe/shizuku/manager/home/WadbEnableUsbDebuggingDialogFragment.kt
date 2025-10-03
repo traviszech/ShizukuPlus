802d1da5afe9580e4ea167ee0971e571c34f80cf
thedjchi <achiommino@gmail.com>
2025-10-02 20:02:34 -0400

    fix dev options dialog

--- manager/src/main/java/moe/shizuku/manager/home/WadbEnableUsbDebuggingDialogFragment.kt
+++ manager/src/main/java/moe/shizuku/manager/home/WadbEnableUsbDebuggingDialogFragment.kt
@@ -15,19 +15,23 @@ class WadbEnableUsbDebuggingDialogFragment :DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val context = requireContext()
-        return MaterialAlertDialogBuilder(context)
+        val dialog = MaterialAlertDialogBuilder(context)
                 .setMessage(R.string.dialog_usb_debugging_not_enabled)
                 .setPositiveButton(R.string.development_settings, null)
                 .setNegativeButton(android.R.string.cancel, null)
                 .create()
+
+        dialog.setOnShowListener { onDialogShow(dialog) }
+        return dialog
     }
 
     private fun onDialogShow(dialog: AlertDialog) {
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
             val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            intent.putExtra(":settings:fragment_args_key", "toggle_adb_wireless")
+            intent.putExtra(":settings:fragment_args_key", "enable_adb")
             try {
+                dismissAllowingStateLoss()
                 it.context.startActivity(intent)
             } catch (_: ActivityNotFoundException) {
             }
