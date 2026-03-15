38ed20fe1d16fc668cfb8fee60dee0727b986e28
thejaustin <the.j.austin@gmail.com>
2026-03-15 03:05:11 -0700

    fix: restore IntentFilter import in RootCompatibilityActivity

--- manager/src/main/java/moe/shizuku/manager/settings/RootCompatibilityActivity.kt
+++ manager/src/main/java/moe/shizuku/manager/settings/RootCompatibilityActivity.kt
@@ -5,6 +5,7 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
